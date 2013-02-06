278cfed10a0581a9aa471eec65c2a5951ca7e86c
bitey <bitey@outlook.com>
2013-02-06 17:47:46 -0600

    Initial Commit

---
+++ newBarTalk/MMAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  MMAppDelegate.h
+//  newBarTalk
+//
+//  Created by StopBitingMe on 2/6/13.
+//  Copyright (c) 2013 StopBitingMe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class MMViewController;
+
+@interface MMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) MMViewController *viewController;
+
+@end
---
+++ newBarTalk/MMViewController.h
@@ -0,0 +1,13 @@
+//
+//  MMViewController.h
+//  newBarTalk
+//
+//  Created by StopBitingMe on 2/6/13.
+//  Copyright (c) 2013 StopBitingMe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMViewController : UIViewController
+
+@end
